29cc7e376b90f236c11cbef8de906b3e74f620e1
Denys Vasylyshyn <denys.mkk@gmail.com>
2025-03-13 18:59:20 +0200

    Add some TODO comments

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from itertools import batched
 
 
 def get_category_height(width_px: float, width_heroes: int, heroes_number: int) -> float:
-    card_width = 51.304352
-    card_height = 82.608699
-    padding2 = 8.695647
+    card_width, card_height, padding2 = (51.304352, 82.608699, 8.695647) # TODO: review & document
     height_heroes = -(-heroes_number//width_heroes) if heroes_number else 1 # div up (min 1)
     height = (card_height*height_heroes+padding2) * width_px / (card_width*width_heroes+padding2)
     return height
@@ -36,7 +34,7 @@ def create_config(config_opts: ConfigSettings, f: dict) -> Config:
     return Config(config_name=config_opts.name, categories=categories)
 
 def main():
-    settings = read_data("settings.json", Settings)
+    settings = read_data("settings.json", Settings) # TODO: argparse
     factory = {
         "file": FileSource(settings.file_source),
         "attr": AttrSource(settings.api_key)
